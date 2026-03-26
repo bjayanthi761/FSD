dd8b431346dd5665b465912dafed87a3a31c3401
bjayanthi761 <bjayanthi761@gmail.com>
2026-03-26 22:30:17 +0530

    Update footer.php

--- LEARNING_ACADEMY/footer.php
+++ LEARNING_ACADEMY/footer.php
@@ -1,7 +1,7 @@
     </main>
     <footer class="footer">
         <div class="container">
-            <p>&copy; 2024 Learning Academy Marketplace. All rights reserved.</p>
+            <p>&copy; 2026 Learning Academy Marketplace. All rights reserved.</p>
         </div>
     </footer>
     <script src="assets/js/main.js"></script>
